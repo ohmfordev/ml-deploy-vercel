7a5cb52ad4b14647d6d3bdde7b7ea021326a2833
ohmfordev <sittiporn.emr@gmail.com>
2024-05-20 20:03:16 +0700

    add setup tools

--- app.py
+++ app.py
@@ -5,18 +5,14 @@ import os
 app = Flask(__name__)
 model = joblib.load('iris_model.pkl')
 
-# GET Data
-
 @app.route("/")
 def start():
     return "The ML Server is Running"
-
 
 @app.route("/index")
 def index():
     return render_template('index.html')
 
-# Body POST
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
