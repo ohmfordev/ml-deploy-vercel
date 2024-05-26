d37d22693e3075efdea5496f7b1b6e34280e2f2f
ohmfordev <sittiporn.emr@gmail.com>
2024-05-26 14:45:32 +0700

    ner deploy

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+import __main__
 from flask import Flask, request, jsonify, render_template
-# import joblib
+import joblib
 import os
+class OnlineLearningModel:
+    pass
 
+__main__.OnlineLearningModel = OnlineLearningModel
 app = Flask(__name__)
-# model = joblib.load('iris_model.pkl')
+model = joblib.load('iris_model.pkl')
 
 @app.route("/")
 def start():
@@ -13,19 +17,21 @@ def start():
 # def index():
 #     return render_template('index.html')
 
-# @app.route('/predict', methods=['POST'])
-# def predict():
-#     try:
-#         data = request.json
-#         sepal_length = data['Sepal length']
-#         sepal_width = data['Sepal width']
-#         petal_length = data['Petal length']
-#         petal_width = data['Petal width']
-#         prediction = model.predict([[sepal_length, sepal_width, petal_length, petal_width]])[0]
-#         return jsonify({'predicted_species': prediction})
-#     except Exception as e:
-#         return jsonify({'error': 'Prediction error: ' + str(e)}), 404
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        data = request.json
+        sepal_length = data['Sepal length']
+        sepal_width = data['Sepal width']
+        petal_length = data['Petal length']
+        petal_width = data['Petal width']
+        prediction = model.predict([[sepal_length, sepal_width, petal_length, petal_width]])[0]
+        return jsonify({'predicted_species': prediction})
+    except Exception as e:
+        return jsonify({'error': 'Prediction error: ' + str(e)}), 404
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 9000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
+# if __name__ == '__main__':
+#     port = int(os.environ.get('PORT', 9000))
+#     app.run(host='0.0.0.0', port=port)
